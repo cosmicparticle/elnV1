5f9627965206717179279ab1c63faff273e45e81
manxihuanchi <546568501@qq.com>
2021-01-19 10:15:48 +0800

    提交

---
+++ src/main/java/cho/carbon/fg/eln/constant/item/MaterialReservationCELNE3756Item.java
@@ -0,0 +1,6 @@
+package cho.carbon.fg.eln.constant.item;
+public class MaterialReservationCELNE3756Item {
+public static final String 物料预定_基本属性组="celn3757";
+public static final String 基本属性组_说明="celn3760";
+public static final String 基本属性组_预定状态="celn3767";
+}
